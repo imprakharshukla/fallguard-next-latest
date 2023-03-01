921247a24e497592049b5ca9f698f15f0c2027e6
Prakhar Shukla <imprakharshukla@gmail.com>
2023-03-01 15:22:07 +0530

    add:f

--- src/pages/api/addIncident.ts
+++ src/pages/api/addIncident.ts
@@ -2,6 +2,7 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import {initFirebase} from "@/pages/api/_firebase/init";
 import admin from "firebase-admin";
+import axios from "axios";
 
 
 export default async function handler(
@@ -36,6 +37,15 @@ export default async function handler(
     const doc = await admin.firestore().collection('devices').doc(body.device_id).get();
     const data = doc.data();
     if (data) {
+        const snap = await admin.firestore().collection('incidents').get()
+        snap.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+            //update the doc fallen to true
+            admin.firestore().collection('incidents').doc(doc.id).update({
+                fallen: false
+            })
+        })
+        res.status(200).json({response: "Added the incident"})
         try {
             await admin.firestore().collection('incidents').add({
                 time: body.time,
@@ -43,6 +53,15 @@ export default async function handler(
                 device_id: body.device_id,
                 fallen: true
             })
+
+            await axios.post("https://api.telegram.org/bot5802433708:AAHPM3_4da2-7PrdPv4-gF_S8__hoiL0HHc/sendMessage", {
+                    chat_id: '919917110',
+                    photo: body.image,
+                    // @ts-ignore
+                    text: `${doc.data().person_name} has fallen down at ${doc.data().address}}. Please respond in time. We are continuously monitoring.`,
+                    parse_mode: "HTML"
+                }
+            )
             res.status(200).json({response: "Added the incident"})
         } catch (e) {
             console.log({error: e})
