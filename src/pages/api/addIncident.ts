98c75bf148f968f40e2413741a0d00503b2201cd
Prakhar Shukla <prakhars117@gmail.com>
2023-12-13 10:45:40 +0530

    fix

--- src/pages/api/addIncident.ts
+++ src/pages/api/addIncident.ts
@@ -18,12 +18,43 @@ export default async function handler(
     } = req.body;
     console.log({body});
         try {
+
+            let imageUrl = "";
+
+            console.log("Uploading to imgbb");
+
+            
+            const form = new FormData();
+            form.append("image", body.image);
+
+            const options = {
+            method: 'POST',
+            url: 'https://api.imgbb.com/1/upload',
+            params: {expiration: '600', key: '75b5951891da0b10b0f54dd649f2f138'},
+            headers: {'Content-Type': 'multipart/form-data; boundary=---011000010111000001101001'},
+            data: '[form]'
+            };
+
+            const imageReq = await axios.request(options);
+
+            console.log({
+                imageReqData: imageReq.data
+            })
+
+            imageUrl = imageReq.data.url;
+            if(imageUrl.length === 0){
+                res.status(400).json({
+                    "error": "No image URL"
+                })
+            }
+
+
             console.log("Sending a message to telegram")
 
             await axios.post(
                 "https://api.telegram.org/bot6713449238:AAGkdF2Z1G-f_bzd-vEQVrPPSjJd8sIscOs/sendPhoto", {
                     chat_id: '773397969',
-                    photo: body.image,
+                    photo: imageUrl,
                     // @ts-ignore
                     caption: `<strong>${body.name}</strong> has been detected at ${body.location} on the device ${body.device_id}. </br> Detection time- ${body.time} </br> Please respond in time. We are continuously monitoring.`,
                     parse_mode: "HTML"
