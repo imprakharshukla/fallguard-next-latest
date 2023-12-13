215a37af03a7d2204a4631d977194c9625a9f582
Prakhar Shukla <prakhars117@gmail.com>
2023-12-13 10:58:42 +0530

    fixed

--- src/pages/api/addIncident.ts
+++ src/pages/api/addIncident.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next'
 import axios from "axios";
-
+import FormData from "form-data";
 
 export default async function handler(
     req: NextApiRequest,
@@ -19,30 +19,31 @@ export default async function handler(
     console.log({body});
         try {
 
+            const apiUrl = 'https://api.imgbb.com/1/upload';
+            const apiKey = '75b5951891da0b10b0f54dd649f2f138';
+
             let imageUrl = "";
 
             console.log("Uploading to imgbb");
 
-            
-            const form = new FormData();
-            form.append("image", body.image);
+            const formData = new FormData();
+            formData.append('image', body.image);
 
-            const options = {
-            method: 'POST',
-            url: 'https://api.imgbb.com/1/upload',
-            params: {expiration: '600', key: '75b5951891da0b10b0f54dd649f2f138'},
-            headers: {'Content-Type': 'multipart/form-data; boundary=---011000010111000001101001'},
-            data: '[form]'
-            };
+            // Axios request
+            const response = await axios.post(`${apiUrl}?expiration=600&key=${apiKey}`, formData, {
+                headers: {
+                    ...formData.getHeaders(),
+                },
+            });
 
-            const imageReq = await axios.request(options);
+
+            imageUrl = response.data.data.url;
 
             console.log({
-                imageReqData: imageReq.data
+                imageUrl
             })
 
-            imageUrl = imageReq.data.url;
-            if(imageUrl.length === 0){
+            if(!imageUrl || imageUrl.length === 0){
                 res.status(400).json({
                     "error": "No image URL"
                 })
@@ -56,7 +57,7 @@ export default async function handler(
                     chat_id: '773397969',
                     photo: imageUrl,
                     // @ts-ignore
-                    caption: `<strong>${body.name}</strong> has been detected at ${body.location} on the device ${body.device_id}. </br> Detection time- ${body.time} </br> Please respond in time. We are continuously monitoring.`,
+                    caption: `<strong>${body.name}</strong> has been detected at ${body.location} on the device ${body.device_id}. Detection time-${body.time} Please respond in time. We are continuously monitoring.`,
                     parse_mode: "HTML"
                 }
             )
